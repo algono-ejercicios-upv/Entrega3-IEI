b26ac19b149432196edc9795ea9117928fb02f49
Alejandro Gómez <algono@inf.upv.es>
2019-12-04 12:45:57 +0100

    Added initial files from tutorial

---
+++ pizzeria/src/main/java/pizzeria/ejemplo/Pizzeria.java
@@ -0,0 +1,12 @@
+package pizzeria.ejemplo;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Pizzeria Funcionando")
+public class Pizzeria extends ServletProcessApplication {
+	
+	// empty implementation
+	// (No tocar (salvo que lo diga el tutorial): necesario para Camunda)
+
+}
