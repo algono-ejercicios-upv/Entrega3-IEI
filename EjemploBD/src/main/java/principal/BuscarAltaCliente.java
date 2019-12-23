a3c2a97a4e0286db5f3574257ea710cb2c7758a7
Alejandro Gómez <algono@inf.upv.es>
2019-12-23 19:08:46 +0100

    Comentario clase main

--- EjemploBD/src/main/java/principal/BuscarAltaCliente.java
+++ EjemploBD/src/main/java/principal/BuscarAltaCliente.java
@@ -5,5 +5,6 @@ import org.camunda.bpm.application.impl.ServletProcessApplication;
 
 @ProcessApplication("Acceso a bases de datos")
 public class BuscarAltaCliente extends ServletProcessApplication {
-
+	// La clase se deja vacia a proposito
+	// La necesita Camunda como "main"
 }
