d474ecd9476b7c02467370265c9cd64d7f3d2b5f
Alejandro Gómez <algono@inf.upv.es>
2019-12-23 18:22:02 +0100

    Fix mispelling buscar alta cliente java

---
+++ EjemploBD/src/main/java/principal/BuscarAltaCliente.java
@@ -0,0 +1,9 @@
+package principal;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Acceso a bases de datos")
+public class BuscarAltaCliente extends ServletProcessApplication {
+
+}
--- EjemploBD/src/main/java/principal/BuscarAltaClliente.java
+++
@@ -1,9 +0,0 @@
-package principal;
-
-import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.application.impl.ServletProcessApplication;
-
-@ProcessApplication("Acceso a bases de datos")
-public class BuscarAltaClliente extends ServletProcessApplication {
-
-}
