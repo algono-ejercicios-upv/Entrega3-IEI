12496f03127830020e713996dfc470ea15b5f19d
Alejandro Gómez <algono@inf.upv.es>
2019-12-26 12:54:16 +0100

    Actualizar insertarCliente a los nuevos campos

--- EjemploBD/src/main/java/bd/InsertarCliente.java
+++ EjemploBD/src/main/java/bd/InsertarCliente.java
@@ -1,5 +1,6 @@
 package bd;
 
+import java.util.Date;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
 
@@ -11,17 +12,19 @@ public class InsertarCliente implements TaskListener {
 
 	@Override
 	public void notify(DelegateTask tareaDelegada) {
-		// TODO Auto-generated method stub
-		// acceso a las variables introducidas en el formulario.
-		String Nombre = (String) tareaDelegada.getExecution().getVariable("IDNombreCliente");
+		// Acceso a las variables introducidas en el formulario
+		String nombre = (String) tareaDelegada.getExecution().getVariable("IDNombreCliente");
 		String email = (String) tareaDelegada.getExecution().getVariable("IDEmail");
-		java.util.Date fechaAlta = (java.util.Date) tareaDelegada.getExecution().getVariable("IDFechaAlta");
-		String NumTarjeta = (String) tareaDelegada.getExecution().getVariable("IDTarjeta");
-		// acceso al tipo enumerado
+		Date fechaAlta = (Date) tareaDelegada.getExecution().getVariable("IDFechaAlta");
+		String numTarjeta = (String) tareaDelegada.getExecution().getVariable("IDTarjeta");
+		
+		// Acceso al tipo enumerado
 		String emisor = (String) tareaDelegada.getExecution().getVariable("IDEmisor");
-		String caducidad = (String) tareaDelegada.getExecution().getVariable("IDCaducidad");
+		String direccion = (String) tareaDelegada.getExecution().getVariable("IDDireccion");
+		
 		ServicioClientes servicio = new ServicioClientes();
-		int idcliente = servicio.insertarCliente(Nombre, email, fechaAlta, NumTarjeta, emisor, caducidad);
+		int idcliente = servicio.insertarCliente(nombre, direccion, fechaAlta, numTarjeta, emisor, email);
+		
 		tareaDelegada.getExecution().setVariable("IDCliente", idcliente);
 
 	}
--- EjemploBD/src/main/java/bd/ServicioClientes.java
+++ EjemploBD/src/main/java/bd/ServicioClientes.java
@@ -34,25 +34,27 @@ public class ServicioClientes {
 	
 	
 	
-	// devuelve la clave con la que se insertó el cliente.
-    public int insertarCliente(String nombre, String Email, java.util.Date FechaAlta, 
-    		                 String TarjetaCredito, String Emisor, String Caducidad) 
+	// Devuelve la clave con la que se insertó el cliente
+    public int insertarCliente(String nombre, String direccion, java.util.Date fechaAlta, 
+    		                 String numTarjeta, String emisor, String correoElectronico)
         {
         int clave = 0;
     	Connection conn = Conexion.abrirConexion();
         if(conn !=null) {
-            String SQL = "INSERT INTO CLIENTE (Nombre, Email, FechaAlta, TarjetaCredito, Emisor, Caducidad, PendientedePago) VALUES (?,?,?,?,?,?,?)";
+            String SQL = "INSERT INTO CLIENTE (Nombre, Direccion, FechaAlta, NumTarjeta, Emisor, Correoelectronico) VALUES (?,?,?,?,?,?)";
             try {
                 PreparedStatement statement = conn.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
-                statement.setString(1,nombre);
-                statement.setString(2,Email);
-                // convertir java.util.Date a java.sql.Date
-                java.sql.Date sqlDate = new java.sql.Date(FechaAlta.getTime());
-                statement.setDate(3,sqlDate);
-                statement.setString(4,TarjetaCredito);
-                statement.setString(5,Emisor);
-                statement.setString(6,Caducidad);
-                statement.setDouble(7, 0.0);
+                statement.setString(1, nombre);
+                statement.setString(2, direccion);
+                
+                // Convertir java.util.Date a java.sql.Date
+                java.sql.Date sqlDate = new java.sql.Date(fechaAlta.getTime());
+                statement.setDate(3, sqlDate);
+                
+                statement.setString(4, numTarjeta);
+                statement.setString(5, emisor);
+                statement.setString(6, correoElectronico);
+                
                 statement.executeUpdate();
                 ResultSet claves = statement.getGeneratedKeys();
                 claves.next();
