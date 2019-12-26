9771ec43591fc45a25094051749638ebfc41d9f1
Alejandro Gómez <algono@inf.upv.es>
2019-12-26 17:23:48 +0100

    Iniciadas algunas clases logica

--- EjemploBD/src/main/java/logica/Pedido.java
+++ EjemploBD/src/main/java/logica/Pedido.java
@@ -1,5 +1,28 @@
 package logica;
 
 public class Pedido {
+	private java.util.Date fechaPedido;
+	private int idCliente;
 
+	public Pedido(java.util.Date fechaPedido, int idCliente) {
+		super();
+		this.fechaPedido = fechaPedido;
+		this.idCliente = idCliente;
+	}
+
+	public java.util.Date getFechaPedido() {
+		return fechaPedido;
+	}
+
+	public void setFechaPedido(java.util.Date fechaPedido) {
+		this.fechaPedido = fechaPedido;
+	}
+
+	public int getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
 }
