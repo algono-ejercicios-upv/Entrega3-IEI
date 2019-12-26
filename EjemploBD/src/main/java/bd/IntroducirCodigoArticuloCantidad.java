3eae5e8226c6f956240968e37c69583580b6d1f5
Alejandro Gómez <algono@inf.upv.es>
2019-12-26 19:07:12 +0100

    Quitado HashMap Pedido
    Tal vez no haga falta

--- EjemploBD/src/main/java/bd/IntroducirCodigoArticuloCantidad.java
+++ EjemploBD/src/main/java/bd/IntroducirCodigoArticuloCantidad.java
@@ -1,8 +1,6 @@
 package bd;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
@@ -15,8 +13,6 @@ public class IntroducirCodigoArticuloCantidad implements TaskListener {
 	@SuppressWarnings("unused")
 	private static final long serialVersionUID = 1L;
 	
-	public static final Map<Integer, Pedido> pedidosEnCurso = new HashMap<Integer, Pedido>();
-
 	@Override
 	public void notify(DelegateTask tareaDelegada) {
 		// Acceso a las variables introducidas en el formulario
@@ -26,14 +22,11 @@ public class IntroducirCodigoArticuloCantidad implements TaskListener {
 		String pedidoActualVariableName = "IDPedidoActual";
 		Pedido pedidoActual;
 		if (tareaDelegada.getExecution().hasVariable(pedidoActualVariableName)) {
-			int pedidoActualHashCode = ServicioBD.getVariable(tareaDelegada, pedidoActualVariableName);
-			pedidoActual = pedidosEnCurso.get(pedidoActualHashCode);
+			pedidoActual = ServicioBD.getVariable(tareaDelegada, pedidoActualVariableName);
 		} else {
 			int idCliente = Integer.parseInt(ServicioBD.<String>getVariable(tareaDelegada, "IDCliente"));
 			pedidoActual = new Pedido(new Date(), idCliente);
-			int pedidoActualHashCode = pedidoActual.hashCode();
-			pedidosEnCurso.put(pedidoActualHashCode, pedidoActual);
-			tareaDelegada.getExecution().setVariable(pedidoActualVariableName, pedidoActualHashCode);
+			tareaDelegada.getExecution().setVariable(pedidoActualVariableName, pedidoActual);
 		}
 		
 		pedidoActual.getLineasPedido().add(new LineaPedido(articulo, cantidadArticulo, pedidoActual));
